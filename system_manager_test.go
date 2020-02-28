4b5be85ddc04a8c4ddd0258f9224e9c1ab1ad24c
Tyler Bezera <tylerjessilynn@gmail.com>
2020-02-27 21:45:36 -0800

    Fix unit tests for systems"

--- system_manager_test.go
+++ system_manager_test.go
@@ -9,11 +9,15 @@ import (
 
 func TestSystemManager_Systems_Should_Have_No_Entity_At_Start(t *testing.T) {
 	m := ecs.NewSystemManager()
-	assert.That(t, len(m.Systems()), is.Equal(0))
+	systems, _ := m.Systems()
+	assert.That(t, len(systems), is.Equal(0))
 }
 
 type MockSystem struct {}
 
+func (s *MockSystem) Name() string {
+	return "MockSystem"
+}
 func (s *MockSystem) Process(entityManager *ecs.EntityManager) {}
 func (s *MockSystem) Setup()                                   {}
 func (s *MockSystem) Teardown()                                {}
@@ -22,8 +26,9 @@ func TestSystemManager_Systems_Should_Have_One_System_After_Adding_One_System(t 
 	m := ecs.NewSystemManager()
 	s := &MockSystem{}
 	m.Add(s)
-	assert.That(t, len(m.Systems()), is.Equal(1))
-	assert.That(t, m.Systems()[0], is.Equal(s))
+	systems, _ := m.Systems()
+	assert.That(t, len(systems), is.Equal(1))
+	assert.That(t, systems[0], is.Equal(s))
 }
 
 func TestSystemManager_Systems_Should_Have_Two_System_After_Adding_Two_System(t *testing.T) {
@@ -31,7 +36,8 @@ func TestSystemManager_Systems_Should_Have_Two_System_After_Adding_Two_System(t 
 	s1 := &MockSystem{}
 	s2 := &MockSystem{}
 	m.Add(s1, s2)
-	assert.That(t, len(m.Systems()), is.Equal(2))
-	assert.That(t, m.Systems()[0], is.Equal(s1))
-	assert.That(t, m.Systems()[1], is.Equal(s2))
+	systems, _ := m.Systems()
+	assert.That(t, len(systems), is.Equal(2))
+	assert.That(t, systems[0], is.Equal(s1))
+	assert.That(t, systems[1], is.Equal(s2))
 }
